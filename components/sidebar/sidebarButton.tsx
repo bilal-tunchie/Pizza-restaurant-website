7771b9c5e9b4a62d64336dbdecc2bfe5e2c55260
bilal-tunchie <BILALMOHAMMED_TUNCHIE@OUTLOOK.COM>
2024-01-22 12:47:11 +0300

    updating Ui

--- components/sidebar/sidebarButton.tsx
+++ components/sidebar/sidebarButton.tsx
@@ -1,4 +1,4 @@
-
+import { cn } from "@/lib/utils";
 import { Button } from "../ui/button";
 
 interface SidebarButtonProps {
@@ -9,9 +9,9 @@ interface SidebarButtonProps {
 function SidebarButton({label, handleClick, disabled}: SidebarButtonProps) {
     return (
         <div 
-            className="absolute bg-white bottom-0 right-0 w-full border-2 flex justify-center 
-            items-center h-20 shadow-[rgba(0,0,0,0.45)_0px_0px_25px_0px]
-            rounded-t-2xl"
+            className={
+                cn("absolute bg-white bottom-0 right-0 w-full border-2 flex justify-center items-center h-20 shadow-[rgba(0,0,00.45)_0px_0px_25px_0px] rounded-t-2xl", disabled && "cursor-not-allowed")
+            }
         >
             <Button 
                 className="w-[93%] h-12 text-md rounded-md mx-auto select-none"
